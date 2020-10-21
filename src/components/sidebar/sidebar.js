cf23ecd6fb4ac68936fc3d4f5002b5f3fa8cafb8
Egor Kochkin <egurt96@yandex.ru>
2020-10-21 17:04:33 +0300

    update display section and playlist list

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './sidebar.scss'
+import SidebarOption from '../sideBarOption/sideBarOption'
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import QueueMusicIcon from '@material-ui/icons/QueueMusic';
 
-function sidebar() {
+function sidebar({playlists}) {
   return (
     <div className="sidebar">
-      <h1>siderbar</h1>
+      <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="spotify logo" className="sidebar__img"/>
+      <SidebarOption title="Главная" Icon={HomeIcon}/>
+      <SidebarOption title="Поиск" Icon={SearchIcon}/>
+      <SidebarOption title="Моя медиатека" Icon={QueueMusicIcon}/>
+      <br />
+      <strong>ПЛЕЙЛИСТЫ</strong>
+      <hr />
+      {playlists?.map(item => <SidebarOption key={item.id} title={item.name} />)}
     </div>
   )
 }
