16bdbf5ee86280fdc366f1869b59f3e3a66796e9
Egor Kochkin <egurt96@yandex.ru>
2020-10-21 19:31:26 +0300

    change title

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,60 @@
-import React from 'react'
-import './footer.scss'
+import React from "react";
+import "./footer.scss";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import { Slider } from "@material-ui/core";
+
 
 function footer() {
+  let min = 0;
+  let max = 150;
   return (
     <div className="footer">
-      <h1>footer</h1>
+      <div className="footer__left">
+        <img
+          src="https://cdns-images.dzcdn.net/images/cover/6f32f9b776af857cb29374ffe4750e5e/500x500.jpg"
+          alt="album cover"
+          className="footer__albumcover"
+        />
+        <div className="footer__songinfo">
+          <a href="http" className="footer__songinfo__song">
+            Goliath
+          </a>
+          <a href="http" className="footer__songinfo__artist">
+            Woodkid
+          </a>
+        </div>
+      </div>
+      <div className="footer__center">
+        <div className="buttons__controls">
+          <ShuffleIcon className="buttons__controls__button" />
+          <SkipPreviousIcon className="buttons__controls__button" />
+          <PlayCircleOutlineIcon
+            className="buttons__controls__button"
+            fontSize="large"
+            style={{ color: "#fff" }}
+          />
+          <SkipNextIcon className="buttons__controls__button" />
+          <RepeatIcon className="buttons__controls__button" />
+        </div>
+        <div className="timebar">
+          <p className="timebar__start">{min}</p>
+          <Slider
+            min={min}
+            max={max}
+            defaultValue={0}
+            step={1}
+            style={{ width: "90%" }}
+          />
+          <p className="timebar__end">{max}</p>
+        </div>
+      </div>
+      <div className="footer__right"></div>
     </div>
-  )
+  );
 }
 
-export default footer
+export default footer;
